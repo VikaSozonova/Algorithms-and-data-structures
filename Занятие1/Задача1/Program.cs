88b75c359ff1ad7ad71c79a7ee045583b3665f83
VikaSozonova <113028359+VikaSozonova@users.noreply.github.com>
2023-09-19 00:20:47 +0500

    Add files via upload

---
+++ Занятие1/Задача1/Program.cs
@@ -0,0 +1,92 @@
+﻿//решения задача Фибоначчи
+
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+
+static Int32 FibRec(Int32 a)
+{
+    if (a == 1 || a == 2)
+        return 1;
+    else
+        return FibRec(a - 1) + FibRec(a - 2);
+}
+
+static Int32 FibIt1(Int32 a)
+{
+    if (a == 1 || a == 2)
+        return 1;
+    else
+    {
+        Int32 first = 1;
+        Int32 second = 1;
+        for (int i = 3; i <= a; i++)
+        {
+            second = first + second;
+            first = second - first;
+        }
+        return second;
+    }
+}
+
+static Int32 FibIt2(Int32 a, ref Int32[] fibs)
+{
+    if (a <= fibs.Length)
+        return fibs[a - 1];
+    Int32[] temp = new Int32[fibs.Length * 2];
+    for (int i = 0; i < fibs.Length; i++)
+        temp[i] = fibs[i];
+    fibs = temp;
+    for (int i = fibs.Length / 2; i < fibs.Length; i++)
+        fibs[i] = fibs[i - 1] + fibs[i - 2];
+    return fibs[a - 1];
+}
+
+ConsoleKey temp = ConsoleKey.Spacebar;
+Int32[] fibs = new Int32[2];
+fibs[0] = 1;
+fibs[1] = 1;
+Stopwatch time;
+
+do
+{
+    bool isTrue = false;
+    int a = 0;
+    while (!isTrue)
+    {
+        Console.Write("Введите порядковый номер искомого числа Фибоначчи: ");
+        string? str = Console.ReadLine();
+        isTrue = Int32.TryParse(str, out a);
+        if (!isTrue)
+            Console.WriteLine("Введено не число\n");
+        if (a < 1 && isTrue)
+        {
+            Console.WriteLine("Введенное число не может быть порядковым номером\n");
+            isTrue = false;
+        }
+    }
+    Int32 res;
+
+    time = Stopwatch.StartNew();
+    time.Restart();
+    res = FibRec(a);
+    time.Stop();
+
+    Console.WriteLine($"Число Фибоначчи под номером {a} = {res} Было найдено за {time.ElapsedTicks} тиков рекурсивным способом");
+
+    time = Stopwatch.StartNew();
+    time.Restart();
+    res = FibIt1(a);
+    time.Stop();
+
+    Console.WriteLine($"Число Фибоначчи под номером {a} = {res} Было найдено за {time.ElapsedTicks} тиков иттеративным способом");
+
+    time = Stopwatch.StartNew();
+    time.Restart();
+    res = FibIt2(a, ref fibs);
+    time.Stop();
+
+    Console.WriteLine($"Число Фибоначчи под номером {a} = {res} Было найдено за {time.ElapsedTicks} тиков иттеративным способом с массивом");
+
+    Console.WriteLine("\nНажмите любую клавишу для продолжения или Escape для выхода\n");
+    temp = Console.ReadKey(true).Key;
+} while (temp != ConsoleKey.Escape);
---
+++ Занятие1/Задача2/Program.cs
@@ -0,0 +1,152 @@
+﻿// нахождение определителя матрицы
+
+using System.Diagnostics;
+
+Int32[,] matrix;
+
+static Int32 Determinant(Int32[,] matrix)
+{
+    int len = matrix.GetLength(0);
+    if (len == 1)
+        return matrix[0, 0];
+    Int32 temp = 0;
+    Int32[,] sMatrix = new Int32[len - 1, len - 1];
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len - 1; j++)
+        {
+            for (int k = 0; k < i; k++)
+            {
+                sMatrix[j, k] = matrix[j + 1, k];
+
+            }
+            for (int k = i; k < len - 1; k++)
+            {
+                sMatrix[j, k] = matrix[j + 1, k + 1];
+            }
+        }
+        if (i % 2 == 0)
+            temp += matrix[0, i] * Determinant(sMatrix);
+        else
+            temp -= matrix[0, i] * Determinant(sMatrix);
+    }
+    return temp;
+}
+
+static Int32[,] RandomInit(Int32[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i <  matrix.GetLength(0); i++)
+        for (int j = 0; j < matrix.GetLength(0);  j++)
+            matrix[i, j] = rnd.Next(-100, 100);
+    return matrix;
+}
+
+Stopwatch time;
+ConsoleKey temp = ConsoleKey.Spacebar;
+
+do
+{
+    bool isTrue = false;
+    int len = 0;
+    while (!isTrue)
+    {
+        Console.Write("Введите размерность квадратной матрицы: ");
+        string? str = Console.ReadLine();
+        isTrue = Int32.TryParse(str, out len);
+        if (!isTrue)
+            Console.WriteLine("Введено не число\n");
+        if (len < 1 && isTrue)
+        {
+            Console.WriteLine("У матрицы такого порядка не может быть определителя\n");
+            isTrue = false;
+        }
+    }
+    matrix = new Int32[len, len];
+    bool isInit = false;
+    do
+    {
+        int i;
+        isInit = true;
+        Console.Clear();
+        Console.WriteLine("   Сгенерировать матрицу вручную\n" +
+            "   Сгенерировать матрицу рандомно");
+        Checker(out i, 2);
+        if (i == 0)
+            for (int k = 0; k < len; i++)
+                for (int j = 0; j < len; j++)
+                {
+                    isTrue = false;
+                    while (!isTrue)
+                    {
+                        Console.Write($"Введите элемент {k + 1} строки {j + 1} столбца: ");
+                        string? str = Console.ReadLine();
+                        isTrue = Int32.TryParse(str, out matrix[k, j]);
+                        if (!isTrue)
+                            Console.WriteLine("Введено не число\n");
+                    }
+                }
+        if (i == 1)
+            matrix = RandomInit(matrix);
+        else
+        {
+            Console.WriteLine("Неверный ввод");
+            isInit = false;
+        }
+    } while (!isInit);
+
+    time = Stopwatch.StartNew();
+    time.Restart();
+    Int32 det = Determinant(matrix);
+    time.Stop();
+
+
+    Console.WriteLine("\nОпределитель матрицы");
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len - 1; j++)
+            Console.Write(matrix[i, j] + " ");
+        Console.WriteLine(matrix[i, len - 1]);
+    }
+    Console.WriteLine($"Равен {det} и был найден за {time.ElapsedTicks} тиков\n");
+
+    Console.WriteLine("\nНажмите любую клавишу для продолжения или Escape для выхода\n");
+    temp = Console.ReadKey(true).Key;
+} while (temp != ConsoleKey.Escape);
+
+static void Checker(out int numberOfFunction, int count, int startPos = 0, string arrow = "->")
+{
+    string empty = new string(' ', arrow.Length);
+    int i = startPos;
+    numberOfFunction = -1;
+    Console.SetCursorPosition(0, startPos);
+    Console.Write(arrow);
+    ConsoleKey key;
+    do
+    {
+        key = Console.ReadKey(true).Key;
+        switch (key)
+        {
+            case ConsoleKey.DownArrow:
+                Console.SetCursorPosition(0, i);
+                Console.Write(empty);
+                if (i == count + startPos - 1)
+                    i = startPos - 1;
+                Console.SetCursorPosition(0, ++i);
+                Console.Write(arrow);
+                break;
+            case ConsoleKey.UpArrow:
+                Console.SetCursorPosition(0, i);
+                Console.Write(empty);
+                if (i == startPos)
+                    i = count + startPos;
+                Console.SetCursorPosition(0, --i);
+                Console.Write(arrow);
+                break;
+            case ConsoleKey.Enter:
+                Console.Clear();
+                numberOfFunction = i - startPos;
+                break;
+        }
+    } while (numberOfFunction == -1);
+}
